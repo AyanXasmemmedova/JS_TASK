2f093de6f8387a562093340d3103ad3c7e64e01a
AyanXasmemmedova <97992002+AyanXasmemmedova@users.noreply.github.com>
2023-12-12 17:04:48 +0400

    Add files via upload

---
+++ JS TASK/main.js
@@ -0,0 +1,44 @@
+//1
+/* let info=prompt("Eded daxil edin:");
+if(!isNaN(info)){
+    for(let i=1;i<=info;i++){
+        if(i%2==0){
+            console.log(i);
+        }
+    }
+}
+else{
+    alert("Number tipinde deyil");
+} */
+
+//2
+/* let info=prompt("Reqem daxil edin:");
+if(!isNaN(info)){
+    if(info>0){
+    console.log("Musbetdir") ;
+    }
+   else if(info<0){
+    console.log("Menfidir");
+    }
+   else{
+    console.log("Sifirdir");
+    }
+}
+else{
+    alert("Reqem deyil");
+} */
+
+//3
+/* const numbers=[1,6,3,0,2];
+let sum=0;
+for(let i=0;i<numbers.length;i++){
+    sum+=numbers[i];
+}
+console.log(sum); */
+
+//4
+/* let a=prompt("Reng daxil edin:");
+let info= document.querySelector("#text").innerHTML+=a; */
+
+    
+
